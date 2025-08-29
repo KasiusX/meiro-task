fb9a1e05e24e75ec666130ec3948e046c48a6368
Tomáš Knor <tomas.knor159@gmail.com>
2025-08-29 14:27:00 +0200

    Finishing api tests

--- tests/connector/test_connector_api.py
+++ tests/connector/test_connector_api.py
@@ -2,33 +2,29 @@ import os
 from unittest.mock import patch
 from fastapi.testclient import TestClient
 from src.connector.connector_api import app
+from src.exceptions.ConnectorErrorException import ConnectorErrorException
+from src.exceptions.ConnectorInvalidInputException import ConnectorInvalidInputException
 
 client = TestClient(app)
 
 def test_valid_data_file():
     _test_upload("data_100.csv", 200, {"message": "Data send successfully"})
     
-def test_invalid_value_file():
-    _test_upload("data_invalid_value.csv", 200, {"message": "Data send successfully"})
-    
-def test_missing_value_file():
-    _test_upload("data_missing_value.csv", 200, {"message": "Data send successfully"})
-    
-def test_empty_file():
-    _test_upload("data_empty.csv", 400, {"detail": "No valid customers data found"})
-    
 def test_non_csv_file():
     _test_upload("data_non_csv.txt", 400, {"detail": "Uploaded file is not a CSV file."})
     
-def test_missing_header_file():
-    _test_upload("data_missing_header.csv", 400, {"detail": "No valid customers data found"})
+def test_handle_connctor_error_exception():
+    with patch("src.connector.connector_api.handle_csv_file", side_effect=ConnectorErrorException("Connector error")):
+        _test_upload("data_100.csv", 500, {"detail": "Connector error"})
+        
+def test_handle_connctor_invalid_input_exception():
+    with patch("src.connector.connector_api.handle_csv_file", side_effect=ConnectorInvalidInputException("Invalid input")):
+        _test_upload("data_100.csv", 400, {"detail": "Invalid input"})
     
 def _test_upload(file_name: str, expected_status: int, expected_body: dict):
     response = _upload_file(file_name)
     assert response.status_code == expected_status
     assert response.json() == expected_body
-    
-# should test 500
 
 def _upload_file(file_name: str):
     file_path = os.path.join(os.path.dirname(__file__), "..", "test_files", file_name)
